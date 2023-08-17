75289843dfb379c95c049e65ffd949dc76f444bc
Kevyn <kevynsilveira0610@gmail.com>
2023-08-17 08:46:57 -0300

    Feito alteração na estrutura do código.

--- SCRIPTS/SEPARA.py
+++ SCRIPTS/SEPARA.py
@@ -4,8 +4,6 @@ from CONSULTA import compare_db
 from CONSULTA import access_db
 from CONSULTA import close_db
 import time
-from BOTAO import status, stop_processing
-
 
 # Variaveis global
 file_client = "" # Armazena o arquivo do cliente
@@ -18,15 +16,16 @@ directory_rs = r"C:\Users\Kevyn\Documents\Destino\RS" # Diretório de envio rs
 total_processed_files = 0 # Total de arquivos processados
 
 def separate_file(): # Separa e envia os arquivos por estabelecimento
+
     # Chama as variaveis globais
     global cnpj_client, file_client, directory_rs, directory_sc, directory_origin, total_processed_files, establishment, status
-    access_db()
+
+
+    access_db() # Acessa o banco de dados.
+
     try:
         file_list = os.listdir(directory_origin) # Armazena a lista dos arquivos presente na pasta
         for file in file_list: # Faz uma varredura da lista de arquivo, acessando arquivo por arquivo
-
-            if stop_processing:
-                break
 
             if file.endswith('.txt'): # Verifica se é um arquivo .txt
                 origin_file_path = os.path.join(directory_origin, file) # Cria um caminho do arquivo
@@ -55,7 +54,9 @@ def separate_file(): # Separa e envia os arquivos por estabelecimento
                 print(f"O arquivo {file} foi processado!")
 
             print(f"Total de arquivos processados {total_processed_files}")
-        close_db()
+
+        close_db() # Fecha a conexão com o banco de dados.
+
         time.sleep(300)  # Pausa de 5 minutos antes da próxima verificação
     except FileNotFoundError:
         print("O arquivo não foi encontrado!")
