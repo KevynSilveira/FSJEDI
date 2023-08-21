14bc92f145141dd4faf5bc09ad2bd726944a4a1e
Kevyn <kevynsilveira0610@gmail.com>
2023-08-21 13:21:17 -0300

    Comentado os log.

--- SCRIPTS/INTERFACE.py
+++ SCRIPTS/INTERFACE.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk # precisa do comando (pip install customtkinter)
-from LOG import redirect_print
+#from LOG import redirect_print
 from FORMATA_HR import format_time
 from BOTAO import start
 from BOTAO import stop
@@ -22,18 +22,24 @@ def create_frame_main(): # Criando frame principal
     b_stop = ctk.CTkButton(master=frame_main, width=100, height=30, text="Pausar", corner_radius=4, command=stop)
     b_stop.place(x=255, y=10)
 
+    #l_pedido = ctk.CTkLabel(master=frame_main, text="LOG PEDIDO", width=100)
+    #l_pedido.place(x=77, y=45)
+
+    #l_retono = ctk.CTkLabel(master=frame_main, text="LOG RETORNO", width=100)
+    #l_retono.place(x=330, y=45)
+
     # Frame log
-    f_log_request = ctk.CTkTextbox(master=frame_main, width=235 , height=200)
-    f_log_request.place(x=10, y=75)
+    #f_log_request = ctk.CTkTextbox(master=frame_main, width=235 , height=200)
+    #f_log_request.place(x=10, y=75)
     #f_log_request.insert(ctk.END, "Pedido")
-    f_log_request.bind("<Key>", lambda e: "break")
+    #f_log_request.bind("<Key>", lambda e: "break")
 
-    f_log_return = ctk.CTkTextbox(master=frame_main, width=235 , height=200)
-    f_log_return.place(x=255, y=75)
+    #f_log_return = ctk.CTkTextbox(master=frame_main, width=235 , height=200)
+    #f_log_return.place(x=255, y=75)
     #f_log_return.insert(ctk.END, "Retorno")
-    f_log_return.bind("<Key>", lambda e: "break")
+    #f_log_return.bind("<Key>", lambda e: "break")
 
-    redirect_print(f_log_request, f_log_return)
+    #redirect_print(f_log_request, f_log_return)
 
     l_runtime = ctk.CTkLabel(master=frame_main, text=f"Tempo em execução: {tempo}", width=100)
     l_runtime.place(x=150, y=275)
@@ -45,12 +51,6 @@ def create_frame_main(): # Criando frame principal
         formatted_time = format_time(tempo)
         l_runtime.configure(text=f"Tempo em execução: {formatted_time}")
         frame_main.after(1000, update_time)
-
-    l_pedido = ctk.CTkLabel(master=frame_main, text="LOG PEDIDO", width=100)
-    l_pedido.place(x=77, y=45)
-
-    l_retono = ctk.CTkLabel(master=frame_main, text="LOG RETORNO", width=100)
-    l_retono.place(x=330, y=45)
 
     update_time()
     frame_main.mainloop()
