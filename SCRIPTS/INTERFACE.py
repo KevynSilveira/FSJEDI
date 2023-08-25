25dcf382ae0e2b0dcf1fab78908e5eda298ff7ff
Kevyn <kevynsilveira0610@gmail.com>
2023-08-25 09:41:47 -0300

    Reduzido e ajustado o tamanho do frame

--- SCRIPTS/INTERFACE.py
+++ SCRIPTS/INTERFACE.py
@@ -10,19 +10,19 @@ def create_frame_main(): # Criando frame principal
     global tempo
 
     frame_main = ctk.CTk()
-    frame_main.geometry("500x300") # Defini a largura e altura do frame respectivamente
+    frame_main.geometry("200x140") # Defini a largura e altura do frame respectivamente
     frame_main.title("FSJEDI")
     frame_main.resizable(False, False) # Tira a opção de maximizar
 
     # Criando botões de execução (start, pause, log)
     b_start = ctk.CTkButton(master=frame_main, width=100, height=30, text="Iniciar", corner_radius=4, command=start)
-    b_start.place(x=145, y=10)
+    b_start.place(x=50, y=10)
 
     b_stop = ctk.CTkButton(master=frame_main, width=100, height=30, text="Pausar", corner_radius=4, command=stop)
-    b_stop.place(x=255, y=10)
+    b_stop.place(x=50, y=50)
 
     l_runtime = ctk.CTkLabel(master=frame_main, text=f"Tempo em execução: {tempo}", width=100)
-    l_runtime.place(x=150, y=275)
+    l_runtime.place(x=5, y=100)
 
     def update_time(): # Atualiza o tempo a cada 1 segundo
         global tempo
